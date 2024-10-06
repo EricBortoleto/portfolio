a4751306ef39a81c1fb5202d7d822e8ea2d22eee
EricBortoleto <eric.biazinibortoleto@gmail.com>
2024-10-05 22:36:35 -0300

    fializando troca de tema

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -5,11 +5,13 @@ import { GithubSecao } from './styles'
 const Sobre = () => (
   <section>
     <Titulo fontSize={16}>Sobre mim</Titulo>
-    <Paragrafo tipo="secundario">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam est,
-      asperiores reprehenderit ex aut, non ducimus omnis eligendi commodi nisi
-      delectus molestias, repudiandae at distinctio odio voluptatibus labore
-      officia blanditiis.
+    <Paragrafo tipo="principal">
+      Meu nome é Eric Bortoleto, tenho 30 anos e moro em São Caetano do Sul/SP.
+      Há mais de 1 ano, me aventuro pelo mundo da programação e atualmente estou
+      aprimorando minhas habilidades através do curso de Engenheiro Front-End na
+      EBAC e também na XPe Educação. Estou animado para compartilhar meus
+      projetos e colaborar com outros desenvolvedores da comunidade. Obrigado
+      por visitar o meu perfil! 👋
     </Paragrafo>
     <GithubSecao>
       <img src="https://github-readme-stats-git-masterrstaa-rickstaa.vercel.app/api?username=EricBortoleto&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
